c0fe877f38b6de750bc103dc4714ee0cb91fda5c
Zanella Matteo <79336755+Zanzibarr@users.noreply.github.com>
2021-12-18 15:55:07 +0100

    Exceptions

--- lib/exceptions.h
+++ lib/exceptions.h
@@ -1,8 +1,27 @@
 #ifndef EXCEPTIONS_H
 #define EXCEPTIONS_H
 
-class illegal_move_exception {
+#include <iostream>
+#include <string>
 
+class illegal_move_exception {
+    public:
+        std::string message {"[ILLEGAL MOVE EXCEPTION]"};
+        illegal_move_exception(std::string message_ = "") { what(message_); }
+        void what(std::string message_) {
+            message += ": " + message_ + "\n";
+            std::cout << message;
+        }
+};
+
+class out_of_bounds_exception {
+    public:
+        std::string message {"[OUT OF BOUNDS EXCEPTION]"};
+        out_of_bounds_exception(std::string message_ = "") { what(message_); }
+        void what(std::string message_) {
+            message += ": " + message_ + "\n";
+            std::cout << message;
+        }
 };
 
 #endif
