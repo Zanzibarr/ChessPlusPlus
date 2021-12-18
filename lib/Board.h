0acf26687552f478d7b68013185e62feb8268b53
RickSrick <79211132+RickSrick@users.noreply.github.com>
2021-12-18 16:40:37 +0100

    Update Board.h

--- lib/Board.h
+++ lib/Board.h
@@ -10,7 +10,7 @@ class board {
 		//return piece
 		void get_piece(std::pair<char, int> &position);
 		bool is_in_chessboard(); //check leget A G
-		bool is_reachable(const std::pair<char, int> &position);
+		bool is_reachable(const set side, const std::pair<char, int> &position);
 		void move();
 
 };
