2fe57eaffcdd8fb416bf954ae83810edc35c64af
RickSrick <79211132+RickSrick@users.noreply.github.com>
2021-12-18 15:06:15 +0100

    Update piece.h

--- piece.h
+++ piece.h
@@ -1,10 +1,22 @@
 #ifndef PIECE_H
 #define PIECE_H
 
-struct Piece {
+/*
+ * Interface describing geric piece of chess.
+ * @author: Riccardo Modolo 2009667
+ */
 
-    char type;
+class Piece {
 
+	protected:
+		enum class side {Black, White};
+		side currentSide; 
+		bool firstMove;
+
+	public:
+		virtual bool isValidMove(char curLet, int curNum, char let, int num) const =0;
+		virtual void moved() =0;
+		
 };
 
-#endif
+#endif //PIECE_H
