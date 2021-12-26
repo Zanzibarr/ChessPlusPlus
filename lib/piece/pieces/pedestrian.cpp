860a7505d629e325d582b43963277bcabb7c1b10
RickSrick <79211132+RickSrick@users.noreply.github.com>
2021-12-26 13:36:57 +0100

    Update pedestrian.cpp

--- lib/piece/pieces/pedestrian.cpp
+++ lib/piece/pieces/pedestrian.cpp
@@ -12,8 +12,8 @@ class pedestrian : public piece {
 
 			int side_multiplier = 0;
 
-			if(side == set::Black) side_multiplier = -1;
-			else if(side == set::White) side_multiplier = 1;
+			if(side == set::Black) side_multiplier = 1;
+			else if(side == set::White) side_multiplier = -1;
 
 			for(int i = 0; i < valid_path.size(); i++) {
 				bool normal_condition = (valid_path[i] == _path && distance == (1*side_multiplier));
