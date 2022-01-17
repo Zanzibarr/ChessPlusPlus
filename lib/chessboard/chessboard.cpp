3003b9373a746e7b24192c0d43aecb85128d2b47
RickSrick <79211132+RickSrick@users.noreply.github.com>
2022-01-17 10:06:46 +0100

    prova fix double free

--- lib/chessboard/chessboard.cpp
+++ lib/chessboard/chessboard.cpp
@@ -556,7 +556,7 @@ void chessboard::do_legit(const coords &_start, const coords &_end) {
     bool eaten = !is<empty_tile>(*piece_at_pos(_end));
 
     if (!is<empty_tile>(*eaten_piece)) {
-        delete eaten_piece;
+        //delete eaten_piece;
         eaten_piece = empty_piece;
     }
 
